1b53c5f341c1cdbd425e825cbe65f58b74041503
tris.gero <tristanpgeronimo@gmail.com>
2022-06-28 00:10:16 +0200

    tested post get mappings

--- API_Custom_Queries_02/src/main/java/it/triexercise/API_Custom_Queries_02/controllers/FlightController.java
+++ API_Custom_Queries_02/src/main/java/it/triexercise/API_Custom_Queries_02/controllers/FlightController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 @RestController
@@ -21,20 +22,25 @@ public class FlightController {
     @PostMapping("/addFlights")
     public void insertNumberOfFlights(@RequestParam(required = false) Integer numberOfFlights) {
 
-        for (int i = 0; i < numberOfFlights; i++) {
-            Flight flight = new Flight();
+        if (numberOfFlights == null || numberOfFlights == 0) {
+            System.out.println("YA ");
+            insertNumberOfFlights(100);
+        } else {
+            for (int i = 0; i < numberOfFlights; i++) {
+                Flight flight = new Flight();
 
-            String str = random.ints(48, 123)
-                    .filter(num -> (num < 58 || num > 64) && (num < 91 || num > 96))
-                    .limit(15)
-                    .mapToObj(c -> (char) c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append)
-                    .toString();
+                String str = random.ints(48, 123)
+                        .filter(num -> (num < 58 || num > 64) && (num < 91 || num > 96))
+                        .limit(15)
+                        .mapToObj(c -> (char) c).collect(StringBuffer::new, StringBuffer::append, StringBuffer::append)
+                        .toString();
 
-            flight.setDescription(str);
-            flight.setFromAirport(str);
-            flight.setToAirport(str);
-            flight.setFlightStatus(FlightStatus.values()[new Random().nextInt(FlightStatus.values().length)]);
-            flightRepository.save(flight);
+                flight.setDescription(str);
+                flight.setFromAirport(str);
+                flight.setToAirport(str);
+                flight.setFlightStatus(FlightStatus.values()[new Random().nextInt(FlightStatus.values().length)]);
+                flightRepository.save(flight);
+            }
         }
     }
 
