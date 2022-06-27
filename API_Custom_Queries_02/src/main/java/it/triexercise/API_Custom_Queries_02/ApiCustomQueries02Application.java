2e5982933f895d0d36af9714c076bc132601fa31
Tristan Geronimo <tristanpgeronimo@gmail.com>
2022-06-27 20:09:35 +0200

    Add files via upload

---
+++ API_Custom_Queries_02/src/main/java/it/triexercise/API_Custom_Queries_02/ApiCustomQueries02Application.java
@@ -0,0 +1,13 @@
+package it.triexercise.API_Custom_Queries_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCustomQueries02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCustomQueries02Application.class, args);
+	}
+
+}
